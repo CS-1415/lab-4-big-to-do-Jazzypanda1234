3ae719953444a06803da752d40f4049804b65f24
Mathew Olsen <mathew.olsen@snow.edu>
2024-10-02 12:44:40 -0600

    Created public class TodoList

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿class TodoListApp {
+﻿public class TodoList {
+
+}
+
+class TodoListApp {
     private TodoList _tasks;
     private bool _showHelp = true;
     private bool _insertMode = true;
@@ -12,12 +16,12 @@
         while (!_quit) {
             Console.Clear();
             Display();
-            ProcessUserInput();
+            // ProcessUserInput();
         }
     }
 
     public void Display() {
-        DisplayTasks();
+        // DisplayTasks();
         if (_showHelp) {
             DisplayHelp();
         }
@@ -27,23 +31,23 @@
         Console.WriteLine("----------------------------");
     }
 
-    public string MakeRow(int i) {
-        Task task = _tasks.GetTask(i);
-        string arrow = "  ";
-        if (task == _tasks.CurrentTask) arrow = "->";
-        string check = " ";
-        if (task.Status == CompletionStatus.Done) check = "X";
-        return $"{arrow} [{check}] {task.Title}";
-    }
+    // public string MakeRow(int i) {
+    //     Task task = _tasks.GetTask(i);
+    //     string arrow = "  ";
+    //     if (task == _tasks.CurrentTask) arrow = "->";
+    //     string check = " ";
+    //     if (task.Status == CompletionStatus.Done) check = "X";
+    //     return $"{arrow} [{check}] {task.Title}";
+    // }
 
-    public void DisplayTasks() {
-        DisplayBar();
-        Console.WriteLine("Tasks:");
-        for (int i = 0; i < _tasks.Length; i++) {
-            Console.WriteLine(MakeRow(i));
-        }
-        DisplayBar();
-    }
+    // public void DisplayTasks() {
+    //     DisplayBar();
+    //     Console.WriteLine("Tasks:");
+    //     for (int i = 0; i < _tasks.Length; i++) {
+    //         Console.WriteLine(MakeRow(i));
+    //     }
+    //     DisplayBar();
+    // }
 
     public void DisplayHelp() {
         Console.WriteLine(
@@ -63,54 +67,56 @@
         return Console.ReadLine()!;
     }
 
-    public void ProcessUserInput() {
-        if (_insertMode) {
-            string taskTitle = GetTitle();
-            if (taskTitle.Length == 0) {
-                _insertMode = false;
-            } else {
-                _tasks.Insert(taskTitle);
-            }
-        } else {
-            switch (Console.ReadKey(true).Key) {
-                case ConsoleKey.Escape:
-                    _quit = true;
-                    break;
-                case ConsoleKey.UpArrow:
-                    _tasks.SelectPrevious();
-                    break;
-                case ConsoleKey.DownArrow:
-                    _tasks.SelectNext();
-                    break;
-                case ConsoleKey.LeftArrow:
-                    _tasks.SwapWithPrevious();
-                    break;
-                case ConsoleKey.RightArrow:
-                    _tasks.SwapWithNext();
-                    break;
-                case ConsoleKey.I:
-                    _insertMode = true;
-                    break;
-                case ConsoleKey.E:
-                    _tasks.CurrentTask.Title = GetTitle();
-                    break;
-                case ConsoleKey.H:
-                    _showHelp = !_showHelp;
-                    break;
-                case ConsoleKey.Enter:
-                case ConsoleKey.Spacebar:
-                    _tasks.CurrentTask.ToggleStatus();
-                    break;
-                case ConsoleKey.Delete:
-                case ConsoleKey.Backspace:
-                    _tasks.DeleteSelected();
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-  }
+//     public void ProcessUserInput() {
+//         if (_insertMode) {
+//             string taskTitle = GetTitle();
+//             if (taskTitle.Length == 0) {
+//                 _insertMode = false;
+//             } else {
+//                 _tasks.Insert(taskTitle);
+//             }
+//         } else {
+//             switch (Console.ReadKey(true).Key) {
+//                 case ConsoleKey.Escape:
+//                     _quit = true;
+//                     break;
+//                 case ConsoleKey.UpArrow:
+//                     _tasks.SelectPrevious();
+//                     break;
+//                 case ConsoleKey.DownArrow:
+//                     _tasks.SelectNext();
+//                     break;
+//                 case ConsoleKey.LeftArrow:
+//                     _tasks.SwapWithPrevious();
+//                     break;
+//                 case ConsoleKey.RightArrow:
+//                     _tasks.SwapWithNext();
+//                     break;
+//                 case ConsoleKey.I:
+//                     _insertMode = true;
+//                     break;
+//                 case ConsoleKey.E:
+//                     _tasks.CurrentTask.Title = GetTitle();
+//                     break;
+//                 case ConsoleKey.H:
+//                     _showHelp = !_showHelp;
+//                     break;
+//                 case ConsoleKey.Enter:
+//                 case ConsoleKey.Spacebar:
+//                     _tasks.CurrentTask.ToggleStatus();
+//                     break;
+//                 case ConsoleKey.Delete:
+//                 case ConsoleKey.Backspace:
+//                     _tasks.DeleteSelected();
+//                     break;
+//                 default:
+//                     break;
+//             }
+//         }
+//     }
+ }
+
+  
 
   class Program {
     static void Main() {
--- obj/Debug/net8.0/lab-4-big-to-do-Jazzypanda1234.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-4-big-to-do-Jazzypanda1234.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-4-big-to-do-Jazzypanda1234")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68e621907a734a56b3749b4d8f419b65ceaa0a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b3c612877bff569c083f641164141840d1fc2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-4-big-to-do-Jazzypanda1234")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-4-big-to-do-Jazzypanda1234")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
